e134ce33e73a704c22515ce9a0b97a3eb2aaec69
Johntolu <76501859+Johntolu@users.noreply.github.com>
2021-10-28 10:22:17 -0700

    committed

--- imagequiz/src/components/Home.js
+++ imagequiz/src/components/Home.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useLocation, useHistory } from "react-router-dom";
+
+import { useHistory } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import flowers from '../communication/flowers';
 import Container from "react-bootstrap/esm/Container";
